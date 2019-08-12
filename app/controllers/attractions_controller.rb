06e7330083ffc9975fd07e2278333bd2505bd186
Nate Tan <nktan93@gmail.com>
2019-08-12 22:20:45 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,11 +1,39 @@
 class AttractionsController < ApplicationController
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
     def index
         @attractions = Attraction.all
+        if session[:user_id]
+          @user = User.find(session[:user_id])
+        end
     end
 
     def show
       @attraction = Attraction.find(params[:id])
       @user = User.find(session[:user_id])
       @ride = Ride.new
+    end
+
+    def edit
+      @attraction = Attraction.find(params[:id])
+    end
+
+    def update
+      @attraction = Attraction.find(params[:id])
+      @attraction.update(attraction_params)
+      redirect_to attraction_path(@attraction)
+    end
+
+    private
+
+    def attraction_params
+      params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'attractions#index'
+
   resources :users
   resources :attractions
   resources :rides
+
+  namespace :admin do
+    resources :attractions, only: [:index, :show]
+  end
+
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   post '/signout' => 'sessions#destroy'
-
-  root 'attractions#index'
-
-  resources :users
-  resources :attractions
 end
