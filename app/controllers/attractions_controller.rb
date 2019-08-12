5dca103a4e8404de6431901ec9939279176f608b
Nate Tan <nktan93@gmail.com>
2019-08-12 22:20:13 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -28,7 +28,7 @@ class AttractionsController < ApplicationController
     def update
       @attraction = Attraction.find(params[:id])
       @attraction.update(attraction_params)
-      redirect_to attractions_path(@attraction)
+      redirect_to attraction_path(@attraction)
     end
 
     private
